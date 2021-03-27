416f80f5462576b713396d0a62524997134aafe3
shivam130502 <73218130+shivam130502@users.noreply.github.com>
2021-03-28 01:09:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var randomNum1 = Math.floor(Math.random()*6)+1;
 var randomNum2 = Math.floor(Math.random()*6)+1;
 
-var randomDiceImage1 = "images/dice"+randomNum1+".png";
+var randomDiceImage1 = "dice"+randomNum1+".png";
 
 var img1 = document.querySelectorAll("img")[0];//will select image 1
 img1.setAttribute("src",randomDiceImage1);
 
-var randomDiceImage2 = "images/dice"+randomNum2+".png";
+var randomDiceImage2 = "dice"+randomNum2+".png";
 
 var img2 = document.querySelectorAll("img")[1];//will select image 2
 img2.setAttribute("src",randomDiceImage2);
